925cee7831bb10fbef4a654168d6735cf0a4dd9d
Henrique Fiaminghi <fiaminghihenrique@gmail.com>
2026-08-20 23:31:03 -0300

    Mercado Não Pago

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = { 
+  apiKey: "AIzaSyDnd-QQfGOmNt4H_MwM2LRAO_mWb1c9sx4",
+  authDomain: "site-database-62284.firebaseapp.com",
+  projectId: "site-database-62284",
+  databaseURL:"https://site-database-62284-default-rtdb.firebaseio.com/",
+  storageBucket: "site-database-62284.firebasestorage.app",
+  messagingSenderId: "870599304528",
+  appId: "1:870599304528:web:6681298be8151363cf7855",
+  measurementId: "G-H8JJFYXTC2"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
